6ac31bc5e98c0113a7a6ce7f1670ec8b3bb68d87
libsupthari-joh <libsupthari@gmail.com>
2026-09-01 15:35:47 +0530

    Update app_new.py

--- app_new.py
+++ app_new.py
@@ -17,7 +17,7 @@ from reportlab.lib.pagesizes import A4, landscape
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.units import mm
 from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttf fonts import TTFont
+from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer, Table, TableStyle
 
 st.set_page_config(
